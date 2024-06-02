8cf2f65d3a3321516e9b602ef175a7075cbe3d20
macabo10 <52705407+macabo10@users.noreply.github.com>
2024-06-02 16:41:14 +0700

    Delete app/public directory

--- app/public/wp-content/themes/blockify/vendor/blockify/framework/public/js/animation.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '5b2029371c14f8bc2020');
--- app/public/wp-content/themes/blockify/vendor/blockify/framework/public/js/splide-autoscroll.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '7d8442cbf8d75df612c4');
